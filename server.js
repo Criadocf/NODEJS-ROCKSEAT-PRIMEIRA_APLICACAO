287559c382f247d86906ae2a9e169574231284f7
kajota <kleberjrmmxvi@gmail.com>
2023-02-17 18:42:24 -0300

    PRIMEIRA APLICACAO/ROCKSEAT

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require("http");
+
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'application/json' });
+    
+    response.end("Minha primeira aplicação com NodeJS");
+})
+.listen(4001, console.log("Servidor está rodando na porta 4001"));
